3215a3d5d4f992be6e9c7eca783601fc4245aea0
afonsobaco <afonsobaco@gmail.com>
2021-10-01 23:04:09 -0300

    Create Solution.java

---
+++ prepare/tutorials/30_days_of_code/Day2_Operators/Solution.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+    public static void solve(double meal_cost, int tip_percent, int tax_percent) {
+        double tip = tip_percent/100.00 * meal_cost;
+        double tax = tax_percent/100.00 * meal_cost;
+        System.out.println(Math.round(meal_cost + tax + tip));
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        double meal_cost = Double.parseDouble(bufferedReader.readLine().trim());
+
+        int tip_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int tax_percent = Integer.parseInt(bufferedReader.readLine().trim());
+
+        Result.solve(meal_cost, tip_percent, tax_percent);
+
+        bufferedReader.close();
+    }
+}
