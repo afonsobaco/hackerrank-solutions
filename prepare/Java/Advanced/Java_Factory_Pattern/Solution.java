148acafe478d18040982db5bba70eeed14ecc615
afonsobaco <afonsobaco@gmail.com>
2021-10-10 01:30:13 -0300

    Create Solution.java

---
+++ prepare/Java/Advanced/Java_Factory_Pattern/Solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.security.*;
+
+interface Food {
+  public String getType();
+}
+
+class Pizza implements Food {
+  public String getType() {
+    return "Someone ordered a Fast Food!";
+  }
+}
+
+class Cake implements Food {
+  public String getType() {
+    return "Someone ordered a Dessert!";
+  }
+}
+
+class FoodFactory {
+  public Food getFood(String order) {
+    if (Pizza.class.getSimpleName().equalsIgnoreCase(order)) {
+      return new Pizza();
+    } else {
+      return new Cake();
+    }
+  }
+}
+
+public class Solution {
+
+  public static void main(String args[]) {
+    try {
+      Scanner sc = new Scanner(System.in);
+      //creating the factory
+      FoodFactory foodFactory = new FoodFactory();
+      //factory instantiates an object
+      Food food = foodFactory.getFood(sc.nextLine());
+      System.out.println("The factory returned " + food.getClass());
+      System.out.println(food.getType());
+    } catch (Do_Not_Terminate.ExitTrappedException e) {
+      System.out.println("Unsuccessful Termination!!");
+    }
+  }
+
+}
