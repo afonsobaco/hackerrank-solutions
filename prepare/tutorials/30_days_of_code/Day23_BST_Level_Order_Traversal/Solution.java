cd37c00bfe375cb1d953fb775704839dcc4e27fc
afonsobaco <afonsobaco@gmail.com>
2021-10-27 00:31:37 -0300

    Create Solution.java

---
+++ prepare/tutorials/30_days_of_code/Day23_BST_Level_Order_Traversal/Solution.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+    static LinkedList<Node> queue = new LinkedList<>();
+
+	static void levelOrder(Node root){
+        if(root != null){
+            queue.add(root);
+            while(queue.peek() != null){
+                Node cur = queue.pop();
+                System.out.print(cur.data + " ");
+                if(cur.left != null){
+                    queue.offer(cur.left);
+                }
+                if(cur.right != null){
+                    queue.offer(cur.right);
+                }
+            }
+        }              
+    }
+
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            levelOrder(root);
+        }	
+}
